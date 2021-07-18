2ec0fbb305d6940ccac8659e01d52fb6bb8750b0
Aishwarya-Sarkar <81924922+Aishwarya-Sarkar@users.noreply.github.com>
2021-07-18 22:56:48 +0530

    Update Finding Updated records.py

--- Finding Updated records.py
+++ Finding Updated records.py
@@ -1,3 +1,12 @@
+'''Finding Updated Records
+We have a table with employees and their salaries, however, some of the records are old and contain outdated salary information. 
+Find the current salary of each employee assuming that salaries increase each year. Output their id, first name, last name, department ID, and current salary.
+Order your list by employee ID in ascending order.
+
+Link to question : https://platform.stratascratch.com/coding-question?id=10299&python=1
+'''
+
+
 # Import your libraries
 import pandas as pd
 
