8152bd752613add0cfa9bc20ad3b9fc42aace143
Aishwarya-Sarkar <81924922+Aishwarya-Sarkar@users.noreply.github.com>
2021-07-22 23:56:15 +0530

    Create Salary differences.py

---
+++ Salary differences.py
@@ -0,0 +1,2 @@
+#Write a query that calculates the difference between the highest salaries found in the marketing and engineering departments. Output just the difference in salaries.
+#Link to question: https://platform.stratascratch.com/coding/10308-salaries-differences?python=1
