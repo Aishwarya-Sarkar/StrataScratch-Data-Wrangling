e3f208689798fe2051cbaebb0514388273eb1b37
Aishwarya Sarkar <81924922+Aishwarya-Sarkar@users.noreply.github.com>
2021-07-26 23:24:32 +0530

    Create Employee and Manager Salaries.py

---
+++ Employee and Manager Salaries.py
@@ -0,0 +1,9 @@
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+employee
+
+df=employee.merge(employee, how='inner', left_on='manager_id' , right_on='id')
+
+df[df['salary_x']>df['salary_y']][['first_name_x','salary_x']]
