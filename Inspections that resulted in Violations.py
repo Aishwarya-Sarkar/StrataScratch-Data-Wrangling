a0e31577423891fe73ea91d59914440bc493ed3a
Aishwarya Sarkar <81924922+Aishwarya-Sarkar@users.noreply.github.com>
2021-08-01 03:32:43 +0530

    Update Inspections that resulted in Violations.py

--- Inspections that resulted in Violations.py
+++ Inspections that resulted in Violations.py
@@ -1,5 +1,14 @@
+#Link to Question: https://platform.stratascratch.com/coding/9728-inspections-that-resulted-in-violations?python=1
+#Question: You're given a dataset of health inspections. Count the number of inspections that resulted in a violation for 'Roxanne Cafe' for each year.
+#If an inspection resulted in a violation, there will be a value in the 'violation_id' column. Output the number of inspections by year in ascending order.
+
 # Import your libraries
 import pandas as pd
 
 # Start writing code
-sf_restaurant_health_violations.head()
+sf_restaurant_health_violations
+
+df=sf_restaurant_health_violations[sf_restaurant_health_violations['business_name']=='Roxanne Cafe']
+df['year']=df['inspection_date'].dt.year
+
+df.groupby('year')['violation_id'].count().reset_index().sort_values('violation_id', ascending=True)
