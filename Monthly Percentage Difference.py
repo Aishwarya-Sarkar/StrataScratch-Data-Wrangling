6c7dfc12e6e67b5c84637a35d14a0aa7d9dd7630
Aishwarya Sarkar <81924922+Aishwarya-Sarkar@users.noreply.github.com>
2021-08-07 23:55:09 +0530

    Update Monthly Percentage Difference.py

--- Monthly Percentage Difference.py
+++ Monthly Percentage Difference.py
@@ -1,1 +1,3 @@
-
+#Question: Given a table of purchases by date, calculate the month-over-month percentage change in revenue. The output should include the year-month date (YYYY-MM) and 
+#percentage change, rounded to the 2nd decimal point, and sorted from the beginning of the year to the end of the year. The percentage change column will be populated 
+#from the 2nd month forward and can be calculated as ((this month's revenue - last month's revenue) / last month's revenue)*100.
